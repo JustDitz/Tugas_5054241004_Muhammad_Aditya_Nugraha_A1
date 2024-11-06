3ad35a715476c5ba180e24689f100af1ae169b86
JustDitz <m.aditya.nug107@gmail.com>
2024-11-06 11:19:35 +0700

    -_-

--- Dasprog/Semester_1/Coretan.py
+++ Dasprog/Semester_1/Coretan.py
@@ -206,8 +206,22 @@
 #         dict_sim[n] = sim(n-1) + sim(n-2) + sim(n-3)
 #         return dict_sim[n]
 
+# balik_tabel = []
+
+# def ans(n):
+#     if n > 0:
+#         balik_tabel.insert(0, sim(n))
+#         ans(n-1)
+
 # n = int(input())
 # ans = []
+# print(balik_tabel)
+
+# tabel = []    
+# def ans(n,x):
+#     if fibo(x) <= n:
+#         tabel.insert(0,fibo(x))
+#         ans(n,x+1)
 
 # for i in range(n+1):
 #     ans.append(sim(i)%100)
@@ -240,3 +254,138 @@
 # password(N)
 
 # -------------------------------------------------
+# Soal 2    
+
+# dict_2 = {1:1, 2:5, 3:14}
+
+# def pol(n):
+#     if n in dict_2:
+#         return dict_2[n]
+#     else:
+#         dict_2[n] = pol(n-1) + (n**2)
+#         return dict_2[n]
+    
+# n = int(input())
+# print(pol(n))
+
+# -------------------------------------------------
+# Soal 3
+
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# maxi = float('-inf')
+# mini = float('inf')
+
+# for i in range(n):
+#     if a[i] > (maxi):
+#         maxi = a[i]
+#     if a[i] < (mini):
+#         mini = a[i]
+
+# print(f"max: {maxi}\nmin: {mini}")
+
+# -------------------------------------------------
+# 5tr0nkP4s5w0rD
+
+# password, key = input().split()
+# key = int(key)
+
+# UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# NUMBER = "0123456789"
+
+# save_num = 0
+# upper_condition = 0
+# number_condition = 0
+# for i in range(len(password)):
+#     if password[i] in UPPERCASE:
+#         upper_condition = 1
+#     if password[i] in NUMBER:
+#         save_num += int(password[i])
+#         number_condition = 1
+        
+# if not number_condition:
+#     print("Password needs number")
+# if not upper_condition:
+#     print("Password requires at least one uppercase letter")
+# if save_num != key:
+#     print("Digits in password not equal to key")
+# if not number_condition or not upper_condition or save_num != key:
+#     print("Weak password, fix your mistakes")
+# else:
+#     print("Password is strong, just like you")
+
+# -------------------------------------------------
+# Anthony Belajar Fibonacci
+
+# dict_fibo = {0:0, 1:1, 2:1}
+# def fibo(n):
+#     if n in dict_fibo:
+#         return dict_fibo[n]
+#     else:
+#         dict_fibo[n] = fibo(n-1) + fibo(n-2)
+#         return dict_fibo[n]
+
+# tabel = []    
+# def ans(n,x):
+#     if fibo(x) <= n:
+#         tabel.insert(0,fibo(x))
+#         ans(n,x+1)
+
+# def total(n):
+#     if n == 0:
+#         return tabel[n]
+#     else:
+#         return tabel[n] + total(n-1)
+             
+# ans(18,0)
+# print(tabel,'\n',total(len(tabel)-1))
+
+# -------------------------------------------------
+# Pola aneh
+
+# dict_pass = {1:[1]}
+# def len_string(n):
+#     if n in dict_pass:
+#         return dict_pass[n]
+#     else:
+#         for i in range(2,n+1):
+#             dict_pass[i] = dict_pass[i-1] + [i] + dict_pass[i-1]
+#         return dict_pass[i]
+
+# pola_num = []
+# def total(n):    
+#     if n == 0:
+#         return pola_num[n]
+#     else:
+#         return pola_num[n] + total(n-1)
+
+# def pattern(n):
+#     for i in len_string(n):
+#         for j in range(1,i+1):
+#             pola_num.append(j)
+#     return total(len(pola_num)-1)
+
+# N = int(input())
+# print(pattern(N))
+
+# -------------------------------------------------
+# I want to Play a Game 
+
+# dict_ubin = {1:2, 2:6}
+
+# save = []
+# save.append(2)
+# save.append(6)
+
+# def ubin(n):
+#     if n in dict_ubin:
+#         return dict_ubin[n]
+#     else:
+#         dict_ubin[n] = 3 * ubin(n-1)
+#         save.append(dict_ubin[n])
+#         return dict_ubin[n]
+
+# N = int(input())
+# ubin(N)
+# print(save)
